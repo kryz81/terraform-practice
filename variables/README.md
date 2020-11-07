dd4986e34dac4cb10b8b8d01060f548aa614fa0e
kryz <k@kryz.dev>
2020-11-07 08:54:45 +0100

    variables

--- variables/README.md
+++ variables/README.md
@@ -1,7 +1,47 @@
 ## Variables
 
-**Object**
+**Create simple variable with default value**
 
 ```hcl-terraform
+variable "myvar" {
+  type = number
+  default = 22
+}
+```
 
+**Provide variable value from shell**
+
+```shell script
+terraform apply --var 'http_port=8888'
+```
+
+**Provide variables from file**
+
+```shell script
+terraform apply --var-file vars.env
+```
+
+**Create an object variable**
+
+```hcl-terraform
+variable "myobject" {
+  type = object({
+    port: number
+    message: string
+    enabled: bool
+    tags: map(string)
+  })
+  default = {
+    port: 8888
+  }
+}
+```
+
+**Create a tuple variable**
+
+```hcl-terraform
+variable  "mytuple" {
+  type = tuple([string, number, bool])
+  default = ["My User", 20, true]
+}
 ```
--- variables/main.tf
+++ variables/main.tf
@@ -1,33 +1,76 @@
 provider "aws" {
-  region = "eu-central-1"
+  region  = "eu-central-1"
   profile = "default"
 }
 
 variable "ami" {
-  type = string
-  default = "ami-00a205cb8e06c3c4e"
+  type    = string
+  default = "ami-0c960b947cbb2dd16"
 }
 
-variable "port" {
-  type = number
-  default = 3000
+variable "http_port" {
+  type    = number
+  default = 80
 }
 
-resource "aws_security_group" "mysg" {
+variable "ssh_port" {
+  type    = number
+  default = 22
+}
+
+variable  "mytuple" {
+  type = tuple([string, number, bool])
+  default = ["Test", 39, true]
+}
+
+variable "hello_message" {
+  type    = string
+  default = "Hello from Terraform!"
+}
+
+resource "aws_security_group" "server" {
   ingress {
-    from_port = var.port
-    protocol = "TCP"
-    to_port = var.port
+    from_port   = var.http_port
+    protocol    = "TCP"
+    to_port     = var.http_port
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = var.port
-    protocol = "TCP"
-    to_port = var.port
+    from_port   = var.http_port
+    protocol    = "TCP"
+    to_port     = var.http_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  ingress {
+    from_port   = var.ssh_port
+    protocol    = "TCP"
+    to_port     = var.ssh_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = var.ssh_port
+    protocol    = "TCP"
+    to_port     = var.ssh_port
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "myinstance" {
-  ami = var.ami
-  instance_type = "t2.micro"
-  security_groups = []
+  ami                    = var.ami
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.server.id, aws_security_group.ssh.id]
+  user_data              = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y nginx
+    echo ${var.hello_message} > /usr/share/nginx/html/index.html
+    sudo service nginx restart
+EOF
+}
+
+output "myip" {
+  value = aws_instance.myinstance.public_ip
 }
