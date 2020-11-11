e7e5127deea156a233231ad71f80195e9ca4e2c2
kryz <k@kryz.dev>
2020-11-11 13:14:33 +0100

    commands, functions

---
+++ commands/README.md
@@ -0,0 +1,14 @@
+## Useful commands
+
+**Format all files in current directory und all subdirectories**
+
+```shell script
+terraform fmt -recursive
+```
+
+**Show providers used in current configuration**
+
+```shell script
+terraform providers
+```
+
--- commands/main.tf
+++ commands/main.tf
@@ -8,8 +8,13 @@ resource "aws_vpc" "vpc1" {
     Name : "vpc1"
   }
 }
+
 resource "aws_subnet" "subnet1" {
   cidr_block = "90.0.1.0/24"
-  vpc_id   = aws_vpc.vpc1.id
+  vpc_id     = aws_vpc.vpc1.id
   depends_on = [aws_vpc.vpc1]
+}
+
+output "myoutput" {
+  value = aws_vpc.vpc1.tags.Name
 }
