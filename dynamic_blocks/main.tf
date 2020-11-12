413a2a69aa4a17561e26cd9bf17b2043adb89e95
kryz <k@kryz.dev>
2020-11-12 11:13:24 +0100

    dynamic blocks

--- dynamic_blocks/main.tf
+++ dynamic_blocks/main.tf
@@ -17,11 +17,13 @@ resource "aws_security_group" "sg1" {
   dynamic "ingress" {
     # iterate over sg_rules
     for_each = local.sg_rules
+    # name of variable from each loop (optional)
+    iterator = item
     # for each sg_rule create "ingress" block with the following content:
     content {
-      description = ingress.value.description
-      from_port   = ingress.value.port
-      to_port     = ingress.value.port
+      description = item.value.description
+      from_port   = item.value.port
+      to_port     = item.value.port
       protocol    = "TCP"
       cidr_blocks = ["0.0.0.0/0"]
     }
