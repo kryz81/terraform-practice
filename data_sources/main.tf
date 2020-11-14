aacac5f32807d8c5f12f725bd2f2d6e9476e5894
kryz <k@kryz.dev>
2020-11-14 19:35:31 +0100

    data sources

--- data_sources/main.tf
+++ data_sources/main.tf
@@ -6,6 +6,15 @@ data "aws_vpc" "web" {
   id = "vpc-0ac07b47cb305021f"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = [099720109477]
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+}
+
 output "web_vpc_id" {
   description = "CIDR block of web vpc"
   value       = data.aws_vpc.web.cidr_block
